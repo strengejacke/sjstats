740db7ba0424a8439fedaeaea0850ab2255e6313
Daniel <mail@danielluedecke.de>
2017-07-15 21:54:18 +0200

    add mode-option to typical_value()

--- R/typical.R
+++ R/typical.R
@@ -6,8 +6,10 @@
 #'
 #' @param x A variable.
 #' @param fun Character vector, naming the function to be applied to numeric
-#'        \code{x}. Currently, \code{"mean"}, \code{"weighted.mean"} and
-#'        \code{"median"} are supported, which call the corresponding R functions.
+#'        \code{x}. Currently, \code{"mean"}, \code{"weighted.mean"},
+#'        \code{"median"} and \code{"mode"} are supported, which call the
+#'        corresponding R functions (except \code{"mode"}, which calls an
+#'        internal function to compute the most common value).
 #' @param ... Further arguments, passed down to \code{fun}.
 #'
 #' @return The "typical" value of \code{x}.
@@ -15,7 +17,7 @@
 #' @details By default, for numeric variables, \code{typical_value()} returns the
 #'          mean value of \code{x} (unless changed with the \code{fun}-argument).
 #'          For factors, the reference level and for character vectors, to the
-#'          most common value is returned.
+#'          most common value (mode) is returned.
 
 #'
 #' @examples
@@ -33,13 +35,15 @@
 #' typical_value(x, "weighted.mean", w = wt)
 #'
 #' @export
-typical_value <- function(x, fun = c("mean", "median", "weighted.mean"), ...) {
+typical_value <- function(x, fun = c("mean", "median", "mode", "weighted.mean"), ...) {
   fun <- match.arg(fun)
 
   if (fun == "median")
     myfun <- get("median", asNamespace("stats"))
   if (fun == "weighted.mean")
     myfun <- get("weighted.mean", asNamespace("stats"))
+  if (fun == "mode")
+    myfun <- get("mode_value", asNamespace("sjstats"))
   else
     myfun <- get("mean", asNamespace("base"))
 
@@ -48,7 +52,12 @@ typical_value <- function(x, fun = c("mean", "median", "weighted.mean"), ...) {
   else if (is.factor(x))
     levels(x)[1]
   else {
-    counts <- table(x)
-    names(counts)[max(counts) == counts]
+    mode_value(x)
   }
+}
+
+
+mode_value <- function(x, ...) {
+  counts <- table(x)
+  names(counts)[max(counts) == counts]
 }
