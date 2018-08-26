393b4a5085fa1c1df3e9ba56a3bfdd392b0b99a3
Daniel <mail@danielluedecke.de>
2018-08-26 11:18:09 +0200

    make sure rstan-tests run only locally

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,8 @@
 library(testthat)
 library(sjstats)
 
+if (length(strsplit(packageDescription("sjstats")$Version, "\\.")[[1]]) > 3) {
+  Sys.setenv("RunAllsjstatsTests" = "yes")
+}
+
 test_check("sjstats")
