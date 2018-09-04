3711c5cb5512265f7177deb2da46e6158b472f92
Daniel <mail@danielluedecke.de>
2018-09-04 21:09:51 +0200

    make tests more stable for CRAN

--- tests/testthat/test-anova_stats.R
+++ tests/testthat/test-anova_stats.R
@@ -1,37 +1,35 @@
-stopifnot(require("testthat"),
-          require("sjstats"))
+if (require("testthat") && require("sjstats")) {
+  context("sjstats, anova_stats")
 
-context("sjstats, anova_stats")
+  # fit linear model
+  data(efc)
+  m <- aov(
+    c12hour ~ as.factor(e42dep) + as.factor(c172code) + c160age,
+    data = efc
+  )
 
+  test_that("eta_sq", {
+    eta_sq(m, partial = FALSE)
+    eta_sq(m, partial = TRUE)
+    eta_sq(m, partial = FALSE, ci.lvl = .5, n = 50)
+    eta_sq(m, partial = TRUE, ci.lvl = .6, n = 50)
+  })
 
-# fit linear model
-data(efc)
-m <- aov(
-  c12hour ~ as.factor(e42dep) + as.factor(c172code) + c160age,
-  data = efc
-)
+  test_that("omega_sq", {
+    omega_sq(m, partial = FALSE)
+    omega_sq(m, partial = TRUE)
+    omega_sq(m, partial = FALSE, ci.lvl = .5, n = 50)
+    omega_sq(m, partial = TRUE, ci.lvl = .6, n = 50)
+  })
 
-test_that("eta_sq", {
-  eta_sq(m, partial = FALSE)
-  eta_sq(m, partial = TRUE)
-  eta_sq(m, partial = FALSE, ci.lvl = .5, n = 50)
-  eta_sq(m, partial = TRUE, ci.lvl = .6, n = 50)
-})
+  test_that("cohens_f", {
+    cohens_f(m)
+  })
 
-test_that("omega_sq", {
-  omega_sq(m, partial = FALSE)
-  omega_sq(m, partial = TRUE)
-  omega_sq(m, partial = FALSE, ci.lvl = .5, n = 50)
-  omega_sq(m, partial = TRUE, ci.lvl = .6, n = 50)
-})
-
-test_that("cohens_f", {
-  cohens_f(m)
-})
-
-test_that("anova_stats", {
-  anova_stats(m, digits = 3)
-  anova_stats(m, digits = 5)
-  anova_stats(car::Anova(m, type = 2))
-  anova_stats(car::Anova(m, type = 3))
-})
+  test_that("anova_stats", {
+    anova_stats(m, digits = 3)
+    anova_stats(m, digits = 5)
+    anova_stats(car::Anova(m, type = 2))
+    anova_stats(car::Anova(m, type = 3))
+  })
+}
