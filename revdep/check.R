11a514d74dfa8576e65809c119e51764b8be5955
Daniel Lüdecke <mail@danielluedecke.de>
2019-06-04 13:33:24 +0200

    checks in base-R

--- revdep/check.R
+++ revdep/check.R
@@ -3,3 +3,9 @@ library("devtools")
 revdep_check()
 revdep_check_save_summary()
 revdep_check_print_problems()
+
+# tools::check_packages_in_dir(
+#   dir = "C:/Users/mail/Documents/R/sj",
+#   check_args = c("--as-cran", ""),
+#   reverse = list(repos = getOption("repos")["CRAN"])
+# )
