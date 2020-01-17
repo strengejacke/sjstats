4702ee0700d6ac693ef03298e80fae24bfab0bf3
Daniel <mail@danielluedecke.de>
2020-01-17 14:22:36 +0100

    tests

--- tests/testthat/test-grpmean.R
+++ tests/testthat/test-grpmean.R
@@ -1,30 +1,28 @@
 if (require("testthat") && require("sjstats") && require("dplyr")) {
-  context("sjstats, grpmean")
-
   data(efc)
   efc$weight <- abs(rnorm(n = nrow(efc), mean = 1, sd = .5))
   efc_grouped <- group_by(efc, c172code)
 
-  test_that("grpmean", {
-    grpmean(efc, c12hour, e42dep)
+  test_that("means_by_group", {
+    means_by_group(efc, c12hour, e42dep)
   })
 
-  test_that("grpmean, weighting", {
+  test_that("means_by_group, weighting", {
     w <- "weight"
-    grpmean(efc, c12hour, e42dep, weights = weight)
-    grpmean(efc, c12hour, e42dep, weights = "weight")
-    grpmean(efc, c12hour, e42dep, weights = w)
+    means_by_group(efc, c12hour, e42dep, weights = weight)
+    means_by_group(efc, c12hour, e42dep, weights = "weight")
+    means_by_group(efc, c12hour, e42dep, weights = w)
   })
 
-  test_that("grpmean, grouping", {
-    grpmean(efc_grouped, c12hour, e42dep)
+  test_that("means_by_group, grouping", {
+    means_by_group(efc_grouped, c12hour, e42dep)
   })
 
-  test_that("grpmean, grouped weighting", {
+  test_that("means_by_group, grouped weighting", {
     w <- "weight"
-    grpmean(efc_grouped, c12hour, e42dep, weights = weight)
-    grpmean(efc_grouped, c12hour, e42dep, weights = "weight")
-    grpmean(efc_grouped, c12hour, e42dep, weights = w)
+    means_by_group(efc_grouped, c12hour, e42dep, weights = weight)
+    means_by_group(efc_grouped, c12hour, e42dep, weights = "weight")
+    means_by_group(efc_grouped, c12hour, e42dep, weights = w)
   })
 
 }
