4e7b59f8f8ed03ea686159fc1969d70352041962
Daniel <mail@danielluedecke.de>
2021-01-07 15:58:51 +0100

    Update test-grpmean.R

--- tests/testthat/test-grpmean.R
+++ tests/testthat/test-grpmean.R
@@ -5,14 +5,14 @@ if (require("testthat") && require("sjstats") && require("dplyr")) {
   efc_grouped <- group_by(efc, c172code)
 
   test_that("means_by_group", {
-    expect_equal(means_by_group(efc, c12hour, e42dep), c(9.90909, 17.53778, 34.51961, 75.90132, 42.4384), tolerance = 1e-3)
+    expect_equal(means_by_group(efc, c12hour, e42dep)$mean, c(9.90909, 17.53778, 34.51961, 75.90132, 42.4384), tolerance = 1e-3)
   })
 
   test_that("means_by_group, weighting", {
     w <- "weight"
-    expect_equal(means_by_group(efc, c12hour, e42dep, weights = weight), c(9.43932, 17.28629, 35.16486, 79.23457, 43.0544), tolerance = 1e-3)
-    expect_equal(means_by_group(efc, c12hour, e42dep, weights = "weight"), c(9.43932, 17.28629, 35.16486, 79.23457, 43.0544), tolerance = 1e-3)
-    expect_equal(means_by_group(efc, c12hour, e42dep, weights = w), c(9.43932, 17.28629, 35.16486, 79.23457, 43.0544), tolerance = 1e-3)
+    expect_equal(means_by_group(efc, c12hour, e42dep, weights = weight)$mean, c(9.43932, 17.28629, 35.16486, 79.23457, 43.0544), tolerance = 1e-3)
+    expect_equal(means_by_group(efc, c12hour, e42dep, weights = "weight")$mean, c(9.43932, 17.28629, 35.16486, 79.23457, 43.0544), tolerance = 1e-3)
+    expect_equal(means_by_group(efc, c12hour, e42dep, weights = w)$mean, c(9.43932, 17.28629, 35.16486, 79.23457, 43.0544), tolerance = 1e-3)
   })
 
   test_that("means_by_group, grouping", {
