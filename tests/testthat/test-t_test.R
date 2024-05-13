f074a3e6a367772ca1022f6bb1204ff98fdc4405
Daniel <mail@danielluedecke.de>
2024-05-13 07:40:17 +0200

    add tests

--- tests/testthat/test-t_test.R
+++ tests/testthat/test-t_test.R
@@ -14,4 +14,16 @@ test_that("t_test", {
   expect_snapshot(t_test(efc, "e17age", "e16sex", weights = "weight"))
   expect_snapshot(t_test(efc, c("e17age", "c160age"), weights = "weight"))
   expect_snapshot(t_test(efc, c("e17age", "c160age"), weights = "weight", paired = TRUE))
+
+  out1 <- t_test(efc, "e17age")
+  out2 <- t.test(efc$e17age ~ 1)
+  expect_equal(out1$statistic, out2$statistic, tolerance = 1e-4, ignore_attr = TRUE)
+  expect_equal(out1$p, out2$p.value, tolerance = 1e-4, ignore_attr = TRUE)
+  expect_equal(out1$effect_size, 9.774916, tolerance = 1e-4, ignore_attr = TRUE)
+
+  out1 <- t_test(efc, "e17age", "e16sex")
+  out2 <- t.test(efc$e17age ~ efc$e16sex)
+  expect_equal(out1$statistic, out2$statistic, tolerance = 1e-4, ignore_attr = TRUE)
+  expect_equal(out1$p, out2$p.value, tolerance = 1e-4, ignore_attr = TRUE)
+  expect_equal(out1$effect_size, -0.5641989, tolerance = 1e-4, ignore_attr = TRUE)
 })
